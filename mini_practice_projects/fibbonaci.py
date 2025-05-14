f34d019e033f2930c64e258fe5c4dfbe7a265120
alien-on-earth-afk <xalien909@gmail.com>
2025-05-14 19:28:15 +0530

    fibonacchi series

--- mini_practice_projects/fibbonaci.py
+++ mini_practice_projects/fibbonaci.py
@@ -1,5 +1,6 @@
 #a script for printing fibonacci series
 
+"""
 n = int(input("Enter n: "))
 fibo = 0
 _sum = 1
@@ -8,5 +9,14 @@ for _ in range(n):
     temp = fibo
     fibo = fibo + _sum
     _sum = temp
+"""
+# #^ working
+# #0, 1, 1, 2, 3, 5, 8, 13
 
-#0, 1, 1, 2, 3, 5, 8, 13
+#recurrsion
+def fibo(n):
+    if (n==0 or n==1):
+        return n
+    return fibo(n-2) + fibo(n-1)
+
+print(fibo(3))
