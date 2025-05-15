d45aa9b8e235c6a2823bb85511edb1cd7b39bc0f
alien-on-earth-afk <xalien909@gmail.com>
2025-05-16 00:03:45 +0530

    to convert upper letters to lower and vice versa

---
+++ mini_practice_projects/swapString.py
@@ -0,0 +1,13 @@
+def swap_case(s):
+    sword = ""
+    for i in s:
+        if i.islower():
+            sword = sword + i.upper()
+        else:
+            sword = sword + i.lower()
+    return sword
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
