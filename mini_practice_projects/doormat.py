30167de6a5cccad7f68e6f6b7d4cafe56df2099f
alien-on-earth-afk <xalien909@gmail.com>
2025-05-16 10:40:55 +0530

    hackerank's vincet doormat

---
+++ mini_practice_projects/doormat.py
@@ -0,0 +1,11 @@
+r, c = map(int, input().split())
+
+for i in range(r):
+    if i < r // 2:
+        pattern = '.|.' * (2 * i + 1)
+    elif i == r // 2:
+        pattern = 'WELCOME'
+    else:
+        pattern = '.|.' * (2 * (r - i - 1) + 1)
+    
+    print(pattern.center(c, '-'))
