39317fa0eb8767a4f9ffac7dc31c9809217de8b1
alien-on-earth-afk <xalien909@gmail.com>
2025-05-21 00:15:08 +0530

    better first and last two chars

--- mini_practice_projects/stringFirstLastTwo.py
+++ mini_practice_projects/stringFirstLastTwo.py
@@ -1,10 +1,20 @@
 import textwrap
 
 s = input("Enter the string: ")
+""""
 l = textwrap.wrap(s, 2)
 n = len(l)
 if(n == 1 and len(l[0]) <= 1):
     word = " "
 else:
     word = l[0] + l[n-1]
+print(word)
+"""
+#nice but there gotta be better way
+#wait string slicing lol
+
+if len(s) < 2:
+    word = " "
+else:
+    word = s[:2] + s[len(s)-2:]
 print(word)
