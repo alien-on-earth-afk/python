94f469cc07fc91959b1c8890726dafccdd408565
alien-on-earth-afk <xalien909@gmail.com>
2025-05-20 23:00:07 +0530

    learning dict

---
+++ mini_practice_projects/dict.py
@@ -0,0 +1,12 @@
+sub = {"maths": 12,
+       "hindi": 34,
+       "english": 35,
+       "physics": 9,
+       "computer": 38,
+       "chemistry": 11}
+print(sub)
+print(sub["english"])
+sub["maths"] = 5
+print(sub["maths"])
+print(sub.keys())
+print(sub.values())
