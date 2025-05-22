7c731785310074870a4c6791af568737c6144ffd
alien-on-earth-afk <xalien909@gmail.com>
2025-05-22 18:09:50 +0530

    to encrypt string with caesar encryption

---
+++ practice_py/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+# a python script to encrypt a string with caesar encryption
+print(ord('z'))
+result = ""
+s = input("Enter the string: ").lower()
+shift = int(input("The number of shit: "))
+
+
+for i in range(len(s)): 
+    if s[i].isalpha():
+        if ord(s[i]) < ord('a') + shift:
+            shifted = chr(ord('z') - (ord(s[i]) - ord('a')))
+        else:
+            shifted = chr(ord(s[i]) - shift)
+        result += str(shifted)
+    else:
+        result += s[i]
+
+print(result)
