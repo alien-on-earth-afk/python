720a4cc81e837cf67e3cc1683a3d00ad645589dc
alien-on-earth-afk <xalien909@gmail.com>
2025-05-24 21:05:20 +0530

    updated sum of list

--- practice_py/sum_of_list.py
+++ practice_py/sum_of_list.py
@@ -1,8 +1,9 @@
 #to add the elements of a list...idk why this was so far into the topic
-
+#only numbers woukd be considered now so you cant add a "hello" in the list to crash it 
 l = input("Entter the list: ").split()
 total = 0
 for i in l:
-    total += int(i)
+    if i.isdigit():
+        total += int(i)
 
 print(total)
