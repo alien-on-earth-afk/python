a6189ffd5b2999c4a5cd74779f6919e5dfc8b45f
alien-on-earth-afk <xalien909@gmail.com>
2025-05-24 20:35:30 +0530

    practising datetime

---
+++ practice_py/datetime_01.py
@@ -0,0 +1,15 @@
+import datetime
+
+now = datetime.datetime.now()
+
+s = now.strftime("""
+                 year is %Y
+                 month is %m
+                 day is %d
+                 hour is %H
+                 min is %M
+                 sec is %S""")
+s1 = now.strftime("%Y-%m-%d %H:%M:%S")
+print(s)
+print(s1)
+#this is really cool
