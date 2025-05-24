7e16f8cf2f59ab567b6cd4c682ad396a8da5c4b0
alien-on-earth-afk <xalien909@gmail.com>
2025-05-24 20:54:51 +0530

    to calculte the area and circumference

---
+++ practice_py/circle_area.py
@@ -0,0 +1,9 @@
+#calculate the area of a circle
+import math
+
+r = int(input("Enter the radius(cm): "))
+#pi = 3.14
+area = math.pi * r**2
+c = 2 * math.pi * r
+print(f"Area is {area}")
+print(f"circumfernce is {c}")
